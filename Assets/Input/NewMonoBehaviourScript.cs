78eedff69f6dd4aea0c7ce05a4a09f22c2d10001
MoMo <mohamedmustafa4321@gmail.com>
2025-09-15 01:24:15 +0300

    First commit - Galaxy-Strike

---
+++ Assets/Input/NewMonoBehaviourScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Diagnostics;
+
+public class LaunchEXE : MonoBehaviour
+{
+    public string exePath = "D:/كتايب/منظومات دجو 360/buk_Windows.exe";
+
+    public void OpenEXE()
+    {
+        Process.Start(exePath);
+    }
+}
